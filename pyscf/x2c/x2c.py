d14ddeebc0c6a8fb1cc08cf1fddd24e44dcd42df
CanonYeh <cnyeh@umich.edu>
2021-07-22 13:27:52 -0400

    pbc.x2c.sfx2c1e w/o one-center approximation

--- pyscf/x2c/x2c.py
+++ pyscf/x2c/x2c.py
@@ -54,7 +54,7 @@ class X2C(lib.StreamObject):
     def get_xmol(self, mol=None):
         if mol is None:
             mol = self.mol
-
+        # self.basis is not mol.basis!
         if self.basis is not None:
             xmol = copy.copy(mol)
             xmol.build(False, False, basis=self.basis)
